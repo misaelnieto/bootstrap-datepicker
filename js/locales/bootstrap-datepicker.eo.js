c7fd357bbfa6fc19c31a5ac7826ff801d6d44669
tzicatl <nnieto@noenieto.com>
2013-01-14 15:50:00 -0300

    Esperanto translations [eo]

---
+++ js/locales/bootstrap-datepicker.eo.js
@@ -0,0 +1,14 @@
+/**
+ * Spanish translation for bootstrap-datepicker
+ * Bruno Bonamin <bruno.bonamin@gmail.com>
+ */
+;(function($){
+	$.fn.datepicker.dates['eo'] = {
+		days: ["Dimanĉo", "Lundo", "Mardo", "Merkredo", "Ĵaûdo", "Vendredo", "Sabato", "Dimanĉo"],
+		daysShort: ["Dim", "Lun", "Mar", "Mer", "Ĵaû", "Ven", "Sab", "Dim"],
+		daysMin: ["Di", "Lu", "Ma", "Me", "Ĵa", "Ve", "Sa", "Di"],
+		months: ["Januaro", "Februaro", "Marto", "Aprilo", "Majo", "Junio", "Julio", "Aûgusto", "Septembro", "Octobro", "Novembro", "Decembro"],
+		monthsShort: ["Jan", "Feb", "Mar", "Apr", "Maj", "Jun", "Jul", "Aûg", "Sep", "Oct", "Nov", "Dec"],
+		today: "Hodiaû"
+	};
+}(jQuery));
